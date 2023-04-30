da71d4178fab84506dd80c7026290e5716cab213
bakseck <seck.bakar@ugb.edu.sn>
2023-04-30 07:24:50 +0000

    done

--- main.go
+++ main.go
@@ -1,22 +1,71 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"text/template"
+)
+
+func handlerMain(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "header")
+}
+
+func renderTemplate(w http.ResponseWriter, tmplName string) {
+	templateCache, err := createTemplateCache()
+
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl, ok := templateCache[tmplName+".page.tmpl"]
+
+	if !ok {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	buffer := new(bytes.Buffer)
+	tmpl.Execute(buffer, nil)
+	buffer.WriteTo(w)
+}
+
+const port = ":8080"
 
 func main() {
-	arguments := os.Args[1:]
-	filename := arguments[0]
-	title := arguments[1]
-	description := arguments[2]
-	author := arguments[3]
+	http.HandleFunc("/", handlerMain)
+	
+	fmt.Println("Server start on port :8080")
+	fmt.Println("Please follow this link http://localhost:8080")
+	http.ListenAndServe(port, nil)
+}
 
-	textReadme := 
-	`<h1 align="center">` + title + `<h1>
-	<h2> Description <h2>
-	<p>` + description + `</p>
-	<br>
-	<br>
-	<br>
-	<a href="https://github.com/` + author + `">` + author + `</a>`
+func createTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
 
-	os.WriteFile(filename + ".md", []byte(textReadme), 0666)
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return cache, nil
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		tmpl := template.Must(template.ParseFiles(page))
+
+		layouts, err := filepath.Glob("./templates/layouts/*.layout.tmpl")
+
+		if err != nil {
+			return cache, err
+		}
+
+		if len(layouts) > 0 {
+			tmpl.ParseGlob("./templates/layouts/*.layout.tmpl")
+		}
+
+		cache[name] = tmpl
+	}
+
+	return cache, nil
 }
